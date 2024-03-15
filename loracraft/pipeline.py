fa0e947c30f409ff4395fb598f713ad7937202de
paul <ppierzc@gmail.com>
2024-03-15 19:17:28 +0100

    init: intialize

---
+++ loracraft/pipeline.py
@@ -0,0 +1,13 @@
+from diffusers import AutoPipelineForText2Image
+import torch
+import tomesd
+
+
+def get_pipeline():
+    pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16,
+                                                     variant="fp16")
+    pipe.enable_xformers_memory_efficient_attention()
+    tomesd.apply_patch(pipe, ratio=0.5)
+    pipe.to("cuda")
+
+    return pipe
